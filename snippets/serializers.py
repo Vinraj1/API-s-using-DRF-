f7e10f9c2e139e37100061662cc734b448af879f
Vinod Kumar <vinodkbimmp@gmail.com>
2025-09-15 07:21:17 +0530

    initial commit

---
+++ snippets/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from snippets.models import Snippets
+from django.contrib.auth.models import User
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Snippets
+        fields = ['url', 'id', 'owner', 'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
